1531685cc12583d465919574b00775c30c0ca112
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-06-05 23:52:22 -0500

    Final

--- src/main/java/org/sofka/ejercicios/twelve/Main.java
+++ src/main/java/org/sofka/ejercicios/twelve/Main.java
@@ -4,6 +4,9 @@ import org.jboss.logging.Logger;
 import org.sofka.utils.MyExeption;
 import org.sofka.utils.MyScanner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * class to determine the differences between two words
  *
@@ -13,19 +16,39 @@ import org.sofka.utils.MyScanner;
 public class Main {
     static final Logger logger = Logger.getLogger("logger");
     static MyScanner scanner = MyScanner.getInstance();
+
     public static void main(String[] args) {
         try {
             logger.info("enter your first sentence");
-            String firstSentence= scanner.getString();
+            String firstSentence = scanner.getString();
             logger.info("enter your second sentence");
-            String secondSentence= scanner.getString();
-            compare(firstSentence,secondSentence);
-        }catch (MyExeption myExeption){
-            logger.info("invalid option"+myExeption.getMessage());
+            String secondSentence = scanner.getString();
+            compare(firstSentence, secondSentence);
+        } catch (MyExeption myExeption) {
+            logger.info("invalid option" + myExeption.getMessage());
         }
     }
-    public static  void compare(String firstSentence , String secondSentence){
 
+    /**
+     * method that evaluates repeated values in a sentence
+     *
+     * @param firstSentence  sentence
+     * @param secondSentence sentence
+     */
+    public static void compare(String firstSentence, String secondSentence) {
+        String [] one=firstSentence.split("");
+        String [] two=secondSentence.split("");
+        if (firstSentence.equalsIgnoreCase(secondSentence)) {
+            logger.info("the phrases are the same");
+        } else {
+            int value=(one.length>two.length) ? one.length : two.length;
+            for(int i=0; i<value; i++)
+                if (!one[i].equals(two[i])) {
+                    String allMessenger = "differences between " + firstSentence + " and " + secondSentence + " is "+ one[i];
+                    logger.info(allMessenger);
+                }
 
         }
     }
+
+}
